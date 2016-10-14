3f6c0068cf5157033d9579184d0fd7c53f129b6d
zhangxintian <zhangxintian@hongkingsystem.cn>
2016-10-14 11:39:49 +0800

    获取温度

--- weather.py
+++ weather.py
@@ -1,5 +1,12 @@
-import urllib
-import json
-
-f = urllib.urlopen("http://api.openweathermap.org/data/2.5/weather?q=Cheltenham,uk")
-weather = f.read()
+import urllib.request
+import re
+from urllib.error import URLError
+try:
+    response  = urllib.request.urlopen("http://www.weather.com.cn/weather1d/101280601.shtml")
+except URLError as e:
+    print("Error:", e.reason)
+html = response.read()
+reg_html = re.compile(r'id="hidden_title"')
+found_temperature = reg_html.search(html.decode("utf8"))
+# print(html.decode("utf8"))
+print(html.decode("utf8")[found_temperature.end():found_temperature.end()+34])
